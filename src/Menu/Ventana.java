6688d3041dba5b7c9b25dcfd48fa373c77be089c
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-09-13 10:51:12 -0300

    Clase Ventana
    creación, pero no finalizacion de la clase

---
+++ src/Menu/Ventana.java
@@ -0,0 +1,14 @@
+package Menu;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+	
+	public Ventana() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500,500);
+		setLocation(350,350);
+		getContentPane().setLayout(null);
+	}
+
+}
