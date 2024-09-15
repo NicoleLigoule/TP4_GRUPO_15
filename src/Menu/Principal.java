301cb8d44316ef35d523395cf39c9afb5e2d1df9
AceitunaGuerrera <tomasmunguia300821@hotmail.com>
2024-09-15 19:59:03 -0300

    agregue boton de ejercicio 1

--- src/Menu/Principal.java
+++ src/Menu/Principal.java
@@ -3,7 +3,8 @@ package Menu;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		 Ventana ventana = new Ventana();
 
 	}
 
--- src/Menu/Ventana.java
+++ src/Menu/Ventana.java
@@ -1,14 +1,30 @@
 package Menu;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class Ventana extends JFrame{
-	
-	public Ventana() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,500);
-		setLocation(350,350);
-		
-	}
 
+public class Ventana extends JFrame {
+    
+    public Ventana() {
+     
+    	JFrame frame = new JFrame();
+		
+		frame.setSize(500,500);
+		frame.setLocation(350,350);
+		frame.setTitle("TP4_GRUPO_15");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+
+		
+		JButton button = new JButton();
+		button.setText("Ejercicio 1");
+		button.setBounds(180, 80, 140, 30);
+		
+		frame.setLayout(null);
+		frame.getContentPane().add(button);	
+		frame.setVisible(true);
+    
+	
+    }
 }
