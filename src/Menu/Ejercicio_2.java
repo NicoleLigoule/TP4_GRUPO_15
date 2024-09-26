91cac7c2d2ad66dc9e28cc5a864bd24e149bb0cb
ferrerotute <matiasferrerov@gmail.com>
2024-09-26 01:34:14 -0300

    Fix GridBagConstraints & Combobox
    - Se fixea el instanciamiento de GridBagConstraints.
    - Se agrega el ComboBox solicitado

--- src/Menu/Ejercicio_2.java
+++ src/Menu/Ejercicio_2.java
@@ -12,77 +12,126 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
-import java.awt.GridLayout;
+public class Ejercicio_2 extends Ventana {
+    private JTextField txtNota1, txtNota2, txtNota3, txtPromedio, txtCondicion;
 
-public class Ejercicio_2 extends Ventana{
-	private JTextField txtNota1, txtNota2, txtNota3, txtPromedio, txtCondicion;
+    public Ejercicio_2() {
+        super(false);
 
-	public Ejercicio_2() {
-		super(false);
-		
-		this.setSize(400, 400);
+        this.setSize(400, 400);
         this.setLocation(500, 100);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        
         JPanel panelNotas1 = new JPanel();
-        panelNotas1.setBorder(new TitledBorder(new LineBorder(new Color(153, 180, 209)), "Notas del estudiante", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+        panelNotas1.setBorder(new TitledBorder(new LineBorder(new Color(153, 180, 209)), "Notas del estudiante",
+                TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
         panelNotas1.setBounds(39, 33, 270, 230);
-        panelNotas1.setLayout(new GridBagLayout()); 
+        GridBagLayout gbl_panelNotas1 = new GridBagLayout();
+        gbl_panelNotas1.columnWeights = new double[]{0.0, 1.0};
+        panelNotas1.setLayout(gbl_panelNotas1);
         getContentPane().add(panelNotas1);
 
- 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5); 
-        gbc.fill = GridBagConstraints.HORIZONTAL;
 
-       
+        // Nota 1
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
         panelNotas1.add(new JLabel("Nota 1:"), gbc);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 0);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 1;
+        gbc.gridy = 0;
         txtNota1 = new JTextField(10);
         panelNotas1.add(txtNota1, gbc);
-        
 
+        // Nota 2
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 1;
         panelNotas1.add(new JLabel("Nota 2:"), gbc);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 0);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridy = 1;
         gbc.gridx = 1;
         txtNota2 = new JTextField(10);
         panelNotas1.add(txtNota2, gbc);
 
-
+        // Nota 3
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 2;
         panelNotas1.add(new JLabel("Nota 3:"), gbc);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 0);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 1;
+        gbc.gridy = 2;
         txtNota3 = new JTextField(10);
         panelNotas1.add(txtNota3, gbc);
-
+        
+    	// ComboBox
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        panelNotas1.add(new JLabel("TPS:"), gbc);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        JComboBox comboBox = new JComboBox();
+        comboBox.setModel(new DefaultComboBoxModel(new String[] {"Aprobado", "Desaprobado"}));
+        panelNotas1.add(comboBox, gbc);
 
         JPanel panelResultados = new JPanel();
-        panelResultados.setBorder(new TitledBorder(new LineBorder(new Color(153, 180, 209)), "Notas del estudiante", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+        panelResultados.setBorder(new TitledBorder(new LineBorder(new Color(153, 180, 209)), "Resultados",
+                TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
         panelResultados.setBounds(39, 300, 270, 100);
-        panelResultados.setLayout(new GridBagLayout()); 
+        panelResultados.setLayout(new GridBagLayout());
         getContentPane().add(panelResultados);
 
-
+        // Promedio
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
         panelResultados.add(new JLabel("Promedio:"), gbc);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 1;
-        txtPromedio = new JTextField(10); 
+        gbc.gridy = 0;
+        txtPromedio = new JTextField(10);
         panelResultados.add(txtPromedio, gbc);
 
-
+        // Condición
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 1;
         panelResultados.add(new JLabel("Condición:"), gbc);
+        gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 1;
-        txtCondicion = new JTextField(10); 
+        gbc.gridy = 1;
+        txtCondicion = new JTextField(10);
         panelResultados.add(txtCondicion, gbc);
 
         JButton btnCalcular = new JButton("CALCULAR");
@@ -97,12 +146,10 @@ public class Ejercicio_2 extends Ventana{
         btnSalir.setBounds(320, 210, 130, 30);
         getContentPane().add(btnSalir);
 
-
         this.setVisible(true);
     }
 
     public static void main(String[] args) {
         new Ejercicio_2();
-	}
-
+    }
 }
