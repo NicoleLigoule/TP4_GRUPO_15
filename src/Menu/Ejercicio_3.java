663bce646aad438d68822c2cd9ac362432c5c9c2
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-09-26 20:58:16 -0300

    radiobuttons

--- src/Menu/Ejercicio_3.java
+++ src/Menu/Ejercicio_3.java
@@ -7,6 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import javax.swing.JRadioButton;
 
 public class Ejercicio_3 extends Ventana{
 	
@@ -20,7 +25,26 @@ public class Ejercicio_3 extends Ventana{
         
         JPanel panelNotas1 = new JPanel();
         panelNotas1.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));     
-        panelNotas1.setBounds(38, 20, 400, 30);        
+        panelNotas1.setBounds(15, 20, 428, 48);        
         getContentPane().add(panelNotas1);
+        panelNotas1.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        
+        JLabel lblElige = new JLabel("Elige un sistema operativo");
+        lblElige.setHorizontalAlignment(SwingConstants.CENTER);
+        lblElige.setFont(new Font("Tahoma", Font.BOLD, 13));
+        panelNotas1.add(lblElige);
+        
+        JRadioButton rdbtnNewRadioButton = new JRadioButton("Windows");
+        panelNotas1.add(rdbtnNewRadioButton);
+        
+        JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Mac");
+        panelNotas1.add(rdbtnNewRadioButton_1);
+        
+        JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Linux");
+        panelNotas1.add(rdbtnNewRadioButton_2);
+        
+
+        
+        
 	}
 }
