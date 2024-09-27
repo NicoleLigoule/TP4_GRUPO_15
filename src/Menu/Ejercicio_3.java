94404cbd8f44fc9110fe3320ef9271c4071558b1
AceitunaGuerrera <tomasmunguia300821@hotmail.com>
2024-09-26 21:45:42 -0300

    panel 2

--- src/Menu/Ejercicio_3.java
+++ src/Menu/Ejercicio_3.java
@@ -43,6 +43,28 @@ public class Ejercicio_3 extends Ventana{
         JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Linux");
         panelNotas1.add(rdbtnNewRadioButton_2);
         
+        JPanel panelNotas2 = new JPanel();
+        panelNotas2.setBorder(new TitledBorder(new LineBorder(Color.BLACK)));     
+        panelNotas2.setBounds(38, 60, 400, 150);        
+        getContentPane().add(panelNotas2);
+        panelNotas2.setLayout(null);
+        
+        JLabel lblEspecialidad = new JLabel("Elije una especialidad");
+        lblEspecialidad.setBounds(32, 61, 151, 20);
+        panelNotas2.add(lblEspecialidad);
+        
+        JCheckBox chkBProgra = new JCheckBox("Programacion");
+        chkBProgra.setBounds(210, 22, 139, 29);
+        panelNotas2.add(chkBProgra);
+        
+        JCheckBox checkBAdm = new JCheckBox("Administraci\u00F3n");
+        checkBAdm.setBounds(210, 57, 139, 29);
+        panelNotas2.add(checkBAdm);
+        
+        JCheckBox checkBoxDiseno = new JCheckBox("Dise\u00F1o Gr\u00E1fico");
+        checkBoxDiseno.setBounds(210, 94, 139, 29);
+        panelNotas2.add(checkBoxDiseno);
+        
 
         
         
